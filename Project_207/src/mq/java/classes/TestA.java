a404775981eaee4556d598dbb2f75e2e389567b6
sunilreddyg <sunilreddy.gajjala@gmail.com>
2022-09-15 09:43:52 +0530

    Add files via upload

---
+++ Project_207/src/mq/java/classes/TestA.java
@@ -0,0 +1,9 @@
+package mq.java.classes;
+
+public class TestA 
+{
+	
+	//Public Modifier class
+	//Public Modifier class information can access across the project
+
+}
---
+++ Project_207/src/mq/java/classes/TestB.java
@@ -0,0 +1,9 @@
+package mq.java.classes;
+
+class TestB 
+{
+	
+	//No Modifier class
+	//This class information can reuse with in package.
+
+}
