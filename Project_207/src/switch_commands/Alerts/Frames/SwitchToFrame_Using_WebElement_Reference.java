fa69191965c23243e00eca07105d6ec8ea1fab33
sunilreddyg <sunilreddy.gajjala@gmail.com>
2022-10-21 10:23:04 +0530

    Add files via upload

---
+++ Project_207/src/switch_commands/Alerts/Frames/SwitchToFrame_Using_WebElement_Reference.java
@@ -0,0 +1,41 @@
+package switch_commands.Frames;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SwitchToFrame_Using_WebElement_Reference {
+
+	public static void main(String[] args) throws Exception {
+		
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver=new FirefoxDriver();     //Launching browser method
+		driver.get("https://www.cleartrip.com/account/trips");
+	    driver.manage().window().maximize();
+	    Thread.sleep(5000);
+	    
+	    //Switch to frame using Frame ID
+	   WebElement FrameElement=driver.findElement(By.xpath("//iframe[@class='spinnerMedium']"));
+	   driver.switchTo().frame(FrameElement);
+	  
+	   //Identifying objects under frame..
+	    WebElement Email=driver.findElement(By.xpath("//input[@id='email']"));
+	    Email.sendKeys("info.dsnr@gmail.com");
+	    
+	    WebElement tripId=driver.findElement(By.id("tripidSecond"));
+	    tripId.sendKeys("67897897");
+	    
+	    //Switch Controls back to MainPage
+	    driver.switchTo().defaultContent();
+	    
+	    //Identifying Object Under page
+	    WebElement Home=driver.findElement(By.xpath("//a[@title='Home']"));
+	    Home.click();
+	    
+
+	}
+
+}
